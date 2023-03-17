e6f70ae169a3dac7f5bcaefafabd517008e18818
mmajewsky133 <martin.majewsky13368@est.uca.edu.ni>
2023-03-17 14:00:23 -0600

    Initial commit

---
+++ app/src/main/java/edu/uca/innovatech/investigacionauthxmlyjson/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.uca.innovatech.investigacionauthxmlyjson
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
