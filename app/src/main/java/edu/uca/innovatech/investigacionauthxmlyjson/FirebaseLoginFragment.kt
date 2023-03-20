01111f73e09ab749986263395b88b70586ec1e88
mmajewsky133 <martin.majewsky13368@est.uca.edu.ni>
2023-03-19 22:55:09 -0600

    Login con Firebase (Carlos Fernando)

--- app/src/main/java/edu/uca/innovatech/investigacionauthxmlyjson/FirebaseLoginFragment.kt
+++ app/src/main/java/edu/uca/innovatech/investigacionauthxmlyjson/FirebaseLoginFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
 import edu.uca.innovatech.investigacionauthxmlyjson.databinding.FragmentFirebaseLoginBinding
 
 class FirebaseLoginFragment : Fragment() {
@@ -26,8 +29,32 @@ class FirebaseLoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        init()
+    }
 
+    private fun init() {
+        binding.btnLogin.setOnClickListener {
+            val user = binding.etUsername.text.toString()
+            val pw = binding.etPassword.text.toString()
 
+            println("$user + $pw")
+
+            if (user.isNotEmpty() && pw.isNotEmpty()) {
+                println("tan llenos")
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(user, pw).addOnCompleteListener{
+                    if (it.isSuccessful) {
+                        Toast.makeText(context,"Login correcto...",Toast.LENGTH_LONG).show()
+                        navigateToMenu()
+                    } else {
+                        Toast.makeText(context,"Credenciales incorrectas...",Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun navigateToMenu() {
+        this.findNavController().navigate(R.id.action_firebaseLoginFragment_to_menuFragment)
     }
 
     override fun onDestroyView() {
